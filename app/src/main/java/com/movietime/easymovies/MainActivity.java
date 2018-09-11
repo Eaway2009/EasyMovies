b30eb7d587d44ff3ce25cf3b7438ae909d4735d2
shenyiwei <shenyiwei@xiaoniu66.com>
2018-09-11 12:07:54 +0800

    23以上申请权限

--- app/src/main/java/com/movietime/easymovies/MainActivity.java
+++ app/src/main/java/com/movietime/easymovies/MainActivity.java
@@ -1,15 +1,22 @@
 package com.movietime.easymovies;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xunlei.downloadlib.XLTaskHelper;
 import com.xunlei.downloadlib.parameter.XLTaskInfo;
@@ -25,11 +32,11 @@ public class MainActivity extends Activity {
     Button btnDownload;
     Button btnPlay;
     TextView tvStatus;
-    Handler handler = new Handler(Looper.getMainLooper()){
+    Handler handler = new Handler(Looper.getMainLooper()) {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            if(msg.what == 0) {
+            if (msg.what == 0) {
                 long taskId = (long) msg.obj;
                 XLTaskInfo taskInfo = XLTaskHelper.instance().getTaskInfo(taskId);
                 tvStatus.setText(
@@ -39,10 +46,11 @@ public class MainActivity extends Activity {
                                 + "/s dcdnSpeed:" + convertFileSize(taskInfo.mAdditionalResDCDNSpeed)
                                 + "/s filePath:" + "/sdcard/" + XLTaskHelper.instance().getFileName(inputUrl.getText().toString())
                 );
-                handler.sendMessageDelayed(handler.obtainMessage(0,taskId),1000);
+                handler.sendMessageDelayed(handler.obtainMessage(0, taskId), 1000);
             }
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,26 +63,28 @@ public class MainActivity extends Activity {
         btnDownload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!TextUtils.isEmpty(inputUrl.getText())) {
+                if (!TextUtils.isEmpty(inputUrl.getText())) {
                     long taskId = 0;
                     try {
-                        taskId = XLTaskHelper.instance().addThunderTask(inputUrl.getText().toString(),"/sdcard/",null);
+                        taskId = XLTaskHelper.instance().addThunderTask(inputUrl.getText().toString(), "/sdcard/", null);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    handler.sendMessage(handler.obtainMessage(0,taskId));
+                    handler.sendMessage(handler.obtainMessage(0, taskId));
                 }
             }
         });
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!TextUtils.isEmpty(inputUrl.getText())) {
+                if (!TextUtils.isEmpty(inputUrl.getText())) {
                     XLVideoPlayActivity.intentTo(XLVideoPlayActivity.class, getApplicationContext(), inputUrl.getText().toString(), inputUrl.getText().toString(), 0);
                 }
             }
         });
+        getPermission();
     }
+
     public static String convertFileSize(long size) {
         long kb = 1024;
         long mb = kb * 1024;
@@ -90,5 +100,37 @@ public class MainActivity extends Activity {
             return String.format(f > 100 ? "%.0f K" : "%.1f K", f);
         } else
             return String.format("%d B", size);
+    }
+
+
+    public void getPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED//存储权限组
+                    ) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                }, AllPer);
+            }
+        }
+    }
+
+    private static final int AllPer = 17;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == AllPer) {
+            boolean READ_EXTERNAL_STORAGECheck = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+            if (!READ_EXTERNAL_STORAGECheck) {
+                mToast("没有申请到存储权限，无法下载播放");
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+    public void mToast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
     }
 }
